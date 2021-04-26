740e1af436dc302f50ef329e665d079275695855
janoweb <janoweb@users.noreply.github.com>
2021-04-26 11:39:13 +0100

    creating animiations for NAV

--- script.js
+++ script.js
@@ -3,38 +3,38 @@
 var controller = new ScrollMagic.Controller();
 
 // NAVIGATION
-var scene = new ScrollMagic.Scene({ offset: 200 })
+var sceneNav = new ScrollMagic.Scene({ triggerElement: "#bio", offset: -200})
   .setClassToggle("#nav", "nav-active")
   .addTo(controller);
 
 // Animation Section:
 // letter a:
-var scene = new ScrollMagic.Scene({ offset: 200 })
+var sceneA = new ScrollMagic.Scene({ offset: 200 })
   .setClassToggle("#letterA", "a-active")
   .addTo(controller);
 
-var scene = new ScrollMagic.Scene({ offset: 200 })
+var sceneLe = new ScrollMagic.Scene({ offset: 200 })
   .setClassToggle("#letterLe", "le-active")
   .addTo(controller);
 
-var scene = new ScrollMagic.Scene({ offset: 200 })
+var sceneX1 = new ScrollMagic.Scene({ offset: 200 })
   .setClassToggle("#letterX1", "x1-active")
   .addTo(controller);
 
-var scene = new ScrollMagic.Scene({ offset: 200 })
+var sceneX2 = new ScrollMagic.Scene({ offset: 200 })
   .setClassToggle("#letterX2", "x2-active")
   .addTo(controller);
 
-var scene = new ScrollMagic.Scene({ offset: 200 })
+var sceneHello = new ScrollMagic.Scene({ offset: 200 })
   .setClassToggle("#hello", "hello-active")
   .addTo(controller);
 
-var scene = new ScrollMagic.Scene({ offset: 200 })
+var sceneIam = new ScrollMagic.Scene({ offset: 200 })
   .setClassToggle("#iam", "iam-active")
   .addTo(controller);
 
 // Bio Section
-var scene = new ScrollMagic.Scene({ triggerElement: "#bio", offset: 200 })
+var sceneBioImg = new ScrollMagic.Scene({ triggerElement: "#bio", offset: 200 })
   // trigger a velocity opaticy animation
   .setClassToggle("#bioImg", "bio__img-active")
   .addIndicators() // add indicators (requires plugin)
