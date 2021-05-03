53f4d1f93f97d223ee9e305ecaf3db81d81b5532
janoweb <janoweb@users.noreply.github.com>
2021-05-03 16:49:54 +0100

    adding animation to the contact links

--- script.js
+++ script.js
@@ -1,78 +1,8 @@
 $(document).ready(function () {
   $(this).scrollTop(0);
-  console.log("ready");
 
-  //Create on document ready animations jut if we are in the top of the document
-  if (
-    $(window).scrollTop() == 0 ||
-    $(window).scrollTop() == $(document).height() - $(window).height()
-  ) {
-    $("#hello").animate({ left: "20vw" }, "slow");
-    $("#iam").delay(1000).animate({ left: "30vw" }, "slow");
-    $("#alex").delay(2500).animate({ bottom: "0" }, "slow");
-    $(".bg-hide").css("display", "fixed");
-  }
-
-  //if on screen:
-  $.fn.isOnScreen = function () {
-    var win = $(window);
-
-    var viewport = {
-      top: win.scrollTop(),
-      left: win.scrollLeft(),
-    };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
-
-    var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
-    bounds.bottom = bounds.top + this.outerHeight();
-
-    return !(
-      viewport.right < bounds.left ||
-      viewport.left > bounds.right ||
-      viewport.bottom < bounds.top ||
-      viewport.top > bounds.bottom
-    );
-  };
-
-  // Nav links animation:
-  $(window).scroll(function () {
-    if ($("#bio").isOnScreen() == true) {
-      $("#bioLink").addClass("link-active");
-      $("#workLink").removeClass("link-active");
-      $("#contactLink").removeClass("link-active");
-    }
-    if ($("#work").isOnScreen() == true) {
-      $("#bioLink").removeClass("link-active");
-      $("#workLink").addClass("link-active");
-      $("#contactLink").removeClass("link-active");
-    }
-    if ($("#contact").isOnScreen() == true) {
-      $("#bioLink").removeClass("link-active");
-      $("#workLink").removeClass("link-active");
-      $("#contactLink").addClass("link-active");
-    }
-  });
-
-  // Lests talk array:
-  const list = document.querySelector("#list");
-  const display = (s) => (list.innerText = s);
-  const names = ["talk", "design", "code", "work"];
-  const delayLoop = (fn, delay) => {
-    return (name, i) => {
-      setTimeout(() => {
-        display(name);
-      }, i * 1500);
-    };
-  };
-  $(window).scroll(function () {
-    if ($(window).scrollTop() + $(window).height() > $(document).height() - 1) {
-      names.forEach(delayLoop(display, 10000));
-    }
-  });
-
-  //cursor:
+  // -- GENERAL JS -- //
+  // -- custom cursor -- //
   var mouseX = 0,
     mouseY = 0;
   var xp = 0,
@@ -96,6 +26,97 @@ $(document).ready(function () {
   $(".item__link").hover(function () {
     $("#circle").toggleClass("circle__hovered");
   });
+  // end cursor //
+
+  // -- if on screen -- //
+  $.fn.isOnScreen = function () {
+    var win = $(window);
+
+    var viewport = {
+      top: win.scrollTop(),
+      left: win.scrollLeft(),
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return !(
+      viewport.right < bounds.left ||
+      viewport.left > bounds.right ||
+      viewport.bottom < bounds.top ||
+      viewport.top > bounds.bottom
+    );
+  };
+
+  // ---- SECTION NAV ---- //
+  // Nav links animation:
+  $(window).scroll(function () {
+    if ($("#bio").isOnScreen() == true) {
+      $("#bioLink").addClass("link-active");
+      $("#workLink").removeClass("link-active");
+      $("#contactLink").removeClass("link-active");
+    }
+    if ($("#work").isOnScreen() == true) {
+      $("#bioLink").removeClass("link-active");
+      $("#workLink").addClass("link-active");
+      $("#contactLink").removeClass("link-active");
+    }
+    if ($("#contact").isOnScreen() == true) {
+      $("#bioLink").removeClass("link-active");
+      $("#workLink").removeClass("link-active");
+      $("#contactLink").addClass("link-active");
+    }
+  });
+
+  // ---- SECTIONA ANIMATION ---- //
+  //Create on document ready animations jut if we are in the top of the document
+  if (
+    $(window).scrollTop() == 0 ||
+    $(window).scrollTop() == $(document).height() - $(window).height()
+  ) {
+    $("#hello").animate({ left: "20vw" }, "slow");
+    $("#iam").delay(1000).animate({ left: "30vw" }, "slow");
+    $("#alex").delay(2500).animate({ bottom: "0" }, "slow");
+    $(".bg-hide").css("display", "fixed");
+  }
+
+  // ---- SECTION WORK ---- //
+
+  // Simple paralax:
+  function parallax() {
+    var yPos = 0 - 30 - window.pageYOffset / 3;
+    document.getElementById("bandaLink").style.top = 1000 + yPos + "%";
+    document.getElementById("myStorytellerLink").style.top = 900 + yPos + "%";
+    document.getElementById("musicaYTuLink").style.top = 1100 + yPos + "%";
+    document.getElementById("preparaLink").style.top = 1200 + yPos + "%";
+  }
+
+  window.addEventListener("scroll", function () {
+    parallax();
+  });
+
+  // ---- SECTION CONTACT ---- //
+  // Lests talk array:
+  const list = document.querySelector("#list");
+  const display = (s) => (list.innerText = s);
+  const names = ["talk", "design", "code", "work"];
+  const delayLoop = (fn, delay) => {
+    return (name, i) => {
+      setTimeout(() => {
+        display(name);
+      }, i * 1500);
+    };
+  };
+  $(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 1) {
+      names.forEach(delayLoop(display, 10000));
+    }
+  });
+
+  // Links logo animations:
 
   //Logo movement:
   // $(document).mousemove(function (e) {
@@ -106,22 +127,9 @@ $(document).ready(function () {
   // $('#letterX1').css('background-position', moveX1 + 'px ' + moveY1 + 'px ');
   // $('#letterX2').css('background-position', moveX1 + 'px ' + moveY1 + 'px ');
   // });
-
-  // Simple paralax:
-  function parallax() {
-    var yPos = 0 - 30 - window.pageYOffset / 3;
-    document.getElementById("bandaLink").style.top = 1000 + yPos + "%";
-    document.getElementById("myStorytellerLink").style.top = 900 + yPos + "%";
-    document.getElementById("musicaYTuLink").style.top = 1100 + yPos + "%";
-    document.getElementById("preparaLink").style.top = 900 + yPos + "%";
-  }
-
-  window.addEventListener("scroll", function () {
-    parallax();
-  });
 });
 
-// SCROLL MAGIC:
+// ------------ SCROLL MAGIC: ----------------
 // init controller
 var controller = new ScrollMagic.Controller();
 
@@ -177,17 +185,43 @@ var sceneContactBg = new ScrollMagic.Scene({ triggerElement: "#contact" })
   .setClassToggle("#bgFront", "bg__front-contact")
   .addTo(controller);
 
+var sceneContactLinks = new ScrollMagic.Scene({
+  triggerElement: "#contact",
+  offset: 100,
+})
+  .setClassToggle("#inLink", "contact__link-active")
+  .addIndicators()
+  .addTo(controller);
+
+var sceneContactLinks2 = new ScrollMagic.Scene({
+  triggerElement: "#contact",
+  offset: 200,
+})
+  .setClassToggle("#gitLink", "contact__link-active")
+  .addIndicators()
+  .addTo(controller);
+
+var sceneContactLinks3 = new ScrollMagic.Scene({
+  triggerElement: "#contact",
+  offset: 300,
+})
+  .setClassToggle("#mailLink", "contact__link-active")
+  .addIndicators()
+  .addTo(controller);
+
+var sceneContactLinks4 = new ScrollMagic.Scene({
+  triggerElement: "#contact",
+  offset: 400,
+})
+  .setClassToggle("#phoneLink", "contact__link-active")
+  .addIndicators()
+  .addTo(controller);
+
 var sceneLets = new ScrollMagic.Scene({
   triggerElement: "#contact",
   offset: 200,
 })
   .setClassToggle("#lets", "lets-active")
-  .addTo(controller);
-
-var sceneHello = new ScrollMagic.Scene({
-  triggerElement: "#contact",
-})
-  .setClassToggle("#hello", "display-none")
   .addTo(controller);
 
 var sceneTalk = new ScrollMagic.Scene({
@@ -196,4 +230,11 @@ var sceneTalk = new ScrollMagic.Scene({
 })
   .setClassToggle("#list", "talk-active")
   // .addIndicators()
+  .addTo(controller);
+
+// remove hello element from animation section:
+var sceneHello = new ScrollMagic.Scene({
+  triggerElement: "#contact",
+})
+  .setClassToggle("#hello", "display-none")
   .addTo(controller);
