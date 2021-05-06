b9ce271d64f829f7265eb0a44be5551598de63d5
janoweb <janoweb@users.noreply.github.com>
2021-05-06 17:30:27 +0100

    fixing nav animation when isOnScreen

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ $(document).ready(function () {
   // -- scroll down -- //
   $("#scrollBanner").delay(3000).fadeIn("slow");
   $(".arrow-hide").delay(3100).animate({ bottom: "-20px" }, 800);
-
   // end scroll banner //
 
   // -- if on screen -- //
@@ -44,18 +43,13 @@ $(document).ready(function () {
 
     var viewport = {
       top: win.scrollTop(),
-      left: win.scrollLeft(),
     };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
+    viewport.bottom = viewport.top + win.height() - 200;
 
     var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
     bounds.bottom = bounds.top + this.outerHeight();
 
     return !(
-      viewport.right < bounds.left ||
-      viewport.left > bounds.right ||
       viewport.bottom < bounds.top ||
       viewport.top > bounds.bottom
     );
